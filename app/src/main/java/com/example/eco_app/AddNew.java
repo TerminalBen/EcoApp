87cc68d7b68e37fe85f5ccd6313cf9b2f72a9041
Bento <bentolima100@gmail.com>
2022-02-08 12:10:15 -0100

    Take picture functionality

--- app/src/main/java/com/example/eco_app/AddNew.java
+++ app/src/main/java/com/example/eco_app/AddNew.java
@@ -1,13 +1,34 @@
 package com.example.eco_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.Manifest;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.net.URI;
+import java.util.Objects;
 
 public class AddNew extends AppCompatActivity {
     private Toolbar  mtoolbar;
+    private static final int PERMISSION_CODE = 1000;
+    private static final int IMAGE_CAPTURE_CODE = 1001;
+    ImageView camera;
+    Uri image_uri;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +37,47 @@ public class AddNew extends AppCompatActivity {
         mtoolbar=findViewById(R.id.toolbar_add);
         setSupportActionBar(mtoolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mtoolbar = findViewById(R.id.toolbar_add);
+        setSupportActionBar(mtoolbar);
+        //Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+
+        camera= findViewById(R.id.camera_button);
+
+        camera.setOnClickListener(new View.OnClickListener() {
+        @Override
+          public void onClick(View view) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    if(checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED ||
+                        checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)== PackageManager.PERMISSION_DENIED)
+                    {
+                        String[] permission = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                        requestPermissions(permission,PERMISSION_CODE);
+                    }
+                    else{
+                        // permission already granted
+                        openCamera();
+                    }
+                }
+                else{
+                    // system < marshmellow
+                    openCamera();
+                }
+          }
+      });
+
+
+    }
+
+    private void openCamera() {
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE,"New Picture");
+        values.put(MediaStore.Images.Media.DESCRIPTION,"From Camera");
+        image_uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values);
+        // openCamera Intent
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,image_uri);
+        startActivityForResult(cameraIntent,IMAGE_CAPTURE_CODE);
     }
 
     @Override
@@ -23,5 +85,32 @@ public class AddNew extends AppCompatActivity {
         super.onCreateOptionsMenu(menu);
         getMenuInflater().inflate(R.menu.secondary_menu,menu);
         return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        // this method is called when user accepts or denies permission from the pop up request
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case PERMISSION_CODE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    openCamera();
+                }
+                else{
+                    Toast.makeText(this,"Permission Denied",Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode,Intent data) {
+        //called when image was captured from camera
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK){
+            // set our image captured to our image view
+            camera.setImageURI(image_uri);
+        }
+
     }
 }
