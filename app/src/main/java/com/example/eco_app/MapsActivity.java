21f0746962a77381ff09c6555bc41ae713601fc3
Bento <bentolima100@gmail.com>
2022-02-11 16:44:44 -0100

    MINOR BUGFIXES

--- app/src/main/java/com/example/eco_app/MapsActivity.java
+++ app/src/main/java/com/example/eco_app/MapsActivity.java
@@ -17,8 +17,16 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.eco_app.databinding.ActivityMapsBinding;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -27,10 +35,9 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //MAP BUG IS HERE. iF I COMMENT THESE 3 LINES THE BUG IS GONE.
-        //@NonNull ActivityMapsBinding binding = ActivityMapsBinding.inflate(getLayoutInflater());
-        //setContentView(binding.getRoot());
-        //setContentView(R.layout.activity_maps);
+        @NonNull ActivityMapsBinding binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_maps);
 
         //private ActivityMapsBinding binding;
         Toolbar mtoolbar = findViewById(R.id.toolbar);
@@ -48,9 +55,9 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
-         super.onCreateOptionsMenu(menu);
-         getMenuInflater().inflate(R.menu.main_menu,menu);
-         return true;
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
     }
 
     /**
@@ -70,6 +77,30 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        FirebaseDatabase.getInstance().getReference().child("Data").addValueEventListener(new ValueEventListener() {
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                List<Double> latitudes = new ArrayList<>(Arrays.asList());
+                List<Double> longitudes = new ArrayList<>(Arrays.asList());
+
+                for (DataSnapshot snapshot: dataSnapshot.getChildren()) {
+                    double latt = (double) snapshot.child("latitude").getValue();
+                    double longg = (double) snapshot.child("longitude").getValue();
+                    //store this data in list
+                    latitudes.add(latt);
+                    longitudes.add(longg);
+                }
+                for(int i=0;i<longitudes.size();i++){
+                    LatLng newLocation = new LatLng(latitudes.get(i), longitudes.get(i));
+                    mMap.addMarker(new MarkerOptions()
+                            .position(newLocation));
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                return;
+            }
+        });
 
 
     }
